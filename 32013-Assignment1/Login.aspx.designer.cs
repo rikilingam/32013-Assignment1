c0bd2098f010ecca223349e518ea33fc2da53f9c
JohnQuan <quan.le.john@gmail.com>
2014-03-20 14:24:19 +1100

    Test

--- 32013-Assignment1/Login.aspx.designer.cs
+++ 32013-Assignment1/Login.aspx.designer.cs
@@ -28,6 +28,6 @@ namespace _32013_Assignment1 {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Login Login1;
+        protected global::System.Web.UI.WebControls.Login  Login1;
     }
 }
