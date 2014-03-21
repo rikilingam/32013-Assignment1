8c7becde1d4164c45de3e41017a5588b12db7851
Bikrem <bikrem.singh@yahoo.in>
2014-03-21 11:35:43 +1100

    Added   TestFunction webform and class

---
+++ 32013-Assignment1/TestFunction.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestFunction.aspx.cs" Inherits="_32013_Assignment1.TestFunction" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form2" runat="server">
+        <div>
+            Currency :
+            <asp:TextBox ID="txtCurrency" runat="server"></asp:TextBox>
+            &nbsp;<asp:DropDownList ID="lstCurrency" runat="server" AutoPostBack="True" DataTextField="Select Currency" OnSelectedIndexChanged="lstCurrency_SelectedIndexChanged" OnTextChanged="lstCurrency_TextChanged">
+                <asp:ListItem>Select Currency</asp:ListItem>
+                <asp:ListItem>AUD</asp:ListItem>
+                <asp:ListItem>CNY</asp:ListItem>
+                <asp:ListItem>Euro</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            AUD&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :
+            <asp:Label ID="lblAUD" runat="server" Text="0"></asp:Label>
+            <br />
+            <br />
+            <br />
+            Attach Receipt :
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+            <br />
+        </div>
+    </form>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 32013-Assignment1/TestFunction.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace _32013_Assignment1
+{
+    public partial class TestFunction : System.Web.UI.Page
+    {
+        TestFunctionClass test = new TestFunctionClass();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lstCurrency_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lstCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            run();
+        }
+
+        public void run()
+        {
+            if (txtCurrency.Text == "")
+                MessageBox.Show("Please enter the amount");
+            else if (lstCurrency.SelectedItem.Text == "Select Currency")
+            {
+                MessageBox.Show("Please select the currency");
+                lblAUD.Text = "0";
+            }
+            else
+            {
+                display();
+            }
+        }
+
+        public void display()
+        {
+            string currencyType = lstCurrency.SelectedItem.Text;
+            double currencyValue = Convert.ToDouble(txtCurrency.Text);
+            lblAUD.Text = test.convertCurrency(currencyType, currencyValue);
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string ext = System.IO.Path.GetExtension(FileUpload1.FileName);
+                if (ext == ".pdf")
+                {
+                    string file = FileUpload1.FileName;
+                    FileUpload1.SaveAs(Server.MapPath("~\\Attachments\\") + file);
+                }
+                else
+                {
+                    MessageBox.Show("Only pdf files can be uploaded");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No file selected to upload");
+            }
+        }
+    }
+}
