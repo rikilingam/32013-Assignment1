f9cc769addd5f75af8121891f439e90e8bd9d2f5
riki.lingam@gmail.com <riki.lingam@gmail.com>
2014-03-21 17:01:52 +1100

    added new business logic and dataaccess folders

---
+++ 32013-Assignment1/DataAccess/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThreeAmigos.ExpenseManagement.UserInterface.DataAccess
+{
+    public class Class1
+    {
+    }
+}
