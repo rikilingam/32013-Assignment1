5ef84786a9e0c20013d342506825dc7f1696652c
Riki Lingam <riki.lingam@gmail.com>
2014-03-22 10:09:12 +1100

    Add N Tier Structure

--- ThreeAmigos.ExpenseManagement.DataAccess/Class1.cs
+++ ThreeAmigos.ExpenseManagement.DataAccess/Class1.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ThreeAmigos.ExpenseManagement.UserInterface.DataAccess
+namespace ThreeAmigos.ExpenseManagement.DataAccess
 {
     public class Class1
     {
