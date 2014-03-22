7313f0715668d0916bbe7260dcc49b1e8df494f7
Riki Lingam <riki.lingam@gmail.com>
2014-03-22 11:10:41 +1100

    add business objects

---
+++ ThreeAmigos.ExpenseManagement.BusinessObject/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeAmigos.ExpenseManagement.BusinessObject
+{
+    class Department
+    {
+        private string departmentName;
+        private double monthlyBudget;
+    }
+}
---
+++ ThreeAmigos.ExpenseManagement.BusinessObject/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeAmigos.ExpenseManagement.BusinessObject
+{
+    public abstract class Employee
+    {
+        protected int employeeId;
+        protected string fullname;
+    }
+}
