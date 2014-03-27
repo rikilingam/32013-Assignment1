f44650226798396df7e7d8019cace4b1050a2a2f
Bikrem <bikrem.singh@yahoo.in>
2014-03-28 02:22:08 +1100

    Added ExpenseItem class objects

--- ThreeAmigos.ExpenseManagement.BusinessObject/ExpenseItem.cs
+++ ThreeAmigos.ExpenseManagement.BusinessObject/ExpenseItem.cs
@@ -8,5 +8,14 @@ namespace ThreeAmigos.ExpenseManagement.BusinessObject
 {
     class ExpenseItem
     {
+        public int itemId { get; set; }
+        public DateTime expenseDate { get; set; }
+        public string location{get;set;}
+        public string description{get;set;}
+        public double amount { get; set; }
+        public string currency { get; set; }
+        public double audAmount { get; set; }
+        public string receiptFileName { get; set; }
+        public int ExpenseHeaderId { get; set; }
     }
 }
