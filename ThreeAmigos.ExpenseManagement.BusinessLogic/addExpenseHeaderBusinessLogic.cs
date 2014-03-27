437b0765b903aa1a26d856ff34550847c5fcb722
Bikrem <bikrem.singh@yahoo.in>
2014-03-28 02:36:36 +1100

    Added a class "addExpenseHeaderBusinessLogic"

--- ThreeAmigos.ExpenseManagement.BusinessLogic/addExpenseHeaderBusinessLogic.cs
+++ ThreeAmigos.ExpenseManagement.BusinessLogic/addExpenseHeaderBusinessLogic.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThreeAmigos.ExpenseManagement.BusinessObject;
+using ThreeAmigos.ExpenseManagement.DataAccess;
 
 namespace ThreeAmigos.ExpenseManagement.BusinessLogic
 {
-    public class Class1
+    public class addExpenseHeaderBusinessLogic
     {
+
     }
 }
