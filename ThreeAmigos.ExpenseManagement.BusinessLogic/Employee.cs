37ff698ad26bf0504a560c2854cab605aa3db3c0
Bikrem <bikrem.singh@yahoo.in>
2014-04-04 14:49:40 +1100

    Added new "Employee" class in BusinessLayer

---
+++ ThreeAmigos.ExpenseManagement.BusinessLogic/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreeAmigos.ExpenseManagement.DataAccess;
+
+namespace ThreeAmigos.ExpenseManagement.BusinessLogic
+{
+   public class Employee
+    {
+       DataAccess.Employee emp = new DataAccess.Employee();
+
+        public int fetchUserId(string username)
+        {
+            return emp.fetchUserId(username);
+        }
+    }
+}
