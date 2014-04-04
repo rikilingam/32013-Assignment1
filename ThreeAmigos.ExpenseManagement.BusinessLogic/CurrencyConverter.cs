9c513cd10d4deb4a4f82f56d85dd5accba23e82c
Bikrem <bikrem.singh@yahoo.in>
2014-04-04 14:52:49 +1100

    Added a class "CurrencyConverter" which have method to convert the currency

---
+++ ThreeAmigos.ExpenseManagement.BusinessLogic/CurrencyConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeAmigos.ExpenseManagement.BusinessLogic
+{
+  public class CurrencyConverter
+    {
+        const double cny = 0.178;
+        const double euro = 1.52;
+        public string convertCurrency(string text, double value)
+        {
+            if (text == "AUD")
+            {
+                return value.ToString();
+            }
+            else if (text == "CNY")
+            {
+                return (value * cny).ToString();
+            }
+            else if (text == "Euro")
+            {
+                return (value * euro).ToString();
+            }
+            else
+            {
+                return "0";
+            }
+        }
+    }
+}
