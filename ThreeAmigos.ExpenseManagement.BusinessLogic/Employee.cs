086d48c5ad6ef4132d92ba7a3d1ad845a0b74e80
Bikrem <bikrem.singh@yahoo.in>
2014-04-06 12:44:31 +1000

    Updated "Employee" class

--- ThreeAmigos.ExpenseManagement.BusinessLogic/Employee.cs
+++ ThreeAmigos.ExpenseManagement.BusinessLogic/Employee.cs
@@ -8,10 +8,17 @@ using ThreeAmigos.ExpenseManagement.DataAccess;
 namespace ThreeAmigos.ExpenseManagement.BusinessLogic
 {
    public class Employee
-    {   
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public int DepartmentId { get; set; }
+        public string Role { get; set; }
+        public int UserId { get; set; }  
+
         DataAccess.Employee emp = new DataAccess.Employee();
         
-       public int FetchUserId(string username)
+        public int FetchUserId(string username)
         {
             return emp.FetchUserId(username);
         }
