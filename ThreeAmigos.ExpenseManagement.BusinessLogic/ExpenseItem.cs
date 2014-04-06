bf0083f502f3b1e3ac3e6f6c8e517d5b066b8845
Bikrem <bikrem.singh@yahoo.in>
2014-04-06 12:44:53 +1000

    Updated "ExpenseItem" class

--- ThreeAmigos.ExpenseManagement.BusinessLogic/ExpenseItem.cs
+++ ThreeAmigos.ExpenseManagement.BusinessLogic/ExpenseItem.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ThreeAmigos.ExpenseManagement.BusinessObject;
+
 using ThreeAmigos.ExpenseManagement.DataAccess;
+
 
 namespace ThreeAmigos.ExpenseManagement.BusinessLogic
 {
-    class ExpenseItem
+   public class ExpenseItem
     {
-        public void AddExpenseItem(BusinessObject.ExpenseItem expItem)
+        public int ItemId { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+        public double AudAmount { get; set; }
+        public string ReceiptFileName { get; set; }
+        public int ExpenseHeaderId { get; set; }
+
+        public void AddExpenseItem(DateTime expDate, string location, string description, double amount, string currency, double audAmount, string receiptFileName, int expHeaderId)
         {
-           DataAccess.ExpenseItem item = new DataAccess.ExpenseItem();
-           item.AddExpenseItem(expItem);
+            ExpenseItemDAL item = new ExpenseItemDAL();
+            item.AddExpenseItem(expDate, location, description, amount, currency, audAmount, receiptFileName, expHeaderId);
         }
     }
 }
