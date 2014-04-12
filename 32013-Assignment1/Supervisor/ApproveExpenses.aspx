85cc4bd6962e3e3d316c04a24781be3c64faab00
Bikrem <bikrem.singh@yahoo.in>
2014-04-12 14:23:43 +1000

    UI for Supervisor

--- 32013-Assignment1/Supervisor/ApproveExpenses.aspx
+++ 32013-Assignment1/Supervisor/ApproveExpenses.aspx
@@ -1,8 +1,49 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EM_Default_MasterPage.Master" AutoEventWireup="true" CodeBehind="ApproveExpenses.aspx.cs" Inherits="ThreeAmigos.ExpenseManagement.UserInterface.Supervisor.ApproveExpenses" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EM_Default_MasterPage.Master" AutoEventWireup="true" CodeBehind="ApproveExpenses.aspx.cs" Inherits="ThreeAmigos.ExpenseManagement.UserInterface.Supervisor.ApproveExpenses"  %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <p>
         <br />
         Approve Expenses</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" BackColor="LightGoldenrodYellow" BorderColor="Tan" BorderWidth="1px" CellPadding="2" ForeColor="Black" GridLines="None" OnRowCommand="GridView1_RowCommand" OnRowCancelingEdit="GridView1_RowCancelingEdit" DataKeyNames="ExpenseId">
+            <AlternatingRowStyle BackColor="PaleGoldenrod" />
+            <Columns>
+                <asp:TemplateField HeaderText="Check Items">
+                    <ItemTemplate>
+                        <asp:Button ID="btnItemDetails" runat="server" Text="Check Items" CommandName="Details" CommandArgument='<%#Container.DataItemIndex %>' />
+                        <asp:GridView ID="GridView2" runat="server">
+                        </asp:GridView>
+                        <asp:Button ID="btnCancelItems" runat="server" Text="Cancel" Visible="False" CommandName="Cancel" CommandArgument='<%#Container.DataItemIndex %>' />
+                        <br />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+            <FooterStyle BackColor="Tan" />
+            <HeaderStyle BackColor="Tan" Font-Bold="True" />
+            <PagerStyle BackColor="PaleGoldenrod" ForeColor="DarkSlateBlue" HorizontalAlign="Center" />
+            <SelectedRowStyle BackColor="DarkSlateBlue" ForeColor="GhostWhite" />
+            <SortedAscendingCellStyle BackColor="#FAFAE7" />
+            <SortedAscendingHeaderStyle BackColor="#DAC09E" />
+            <SortedDescendingCellStyle BackColor="#E1DB9C" />
+            <SortedDescendingHeaderStyle BackColor="#C2A47B" />
+        </asp:GridView>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
 </asp:Content>
--- 32013-Assignment1/Supervisor/ApproveExpenses.aspx.cs
+++ 32013-Assignment1/Supervisor/ApproveExpenses.aspx.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using ThreeAmigos.ExpenseManagement.BusinessLogic;
+using ThreeAmigos.ExpenseManagement.BusinessObject;
+using ThreeAmigos.ExpenseManagement.DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace ThreeAmigos.ExpenseManagement.UserInterface.Supervisor
 {
@@ -11,7 +18,70 @@ namespace ThreeAmigos.ExpenseManagement.UserInterface.Supervisor
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            if (!IsPostBack)
+            {
+                ExpenseReportDAL expenseReportDAL = new ExpenseReportDAL();
+                Employee emp = new Employee();
+                EmployeeDAL employeeDAL = new EmployeeDAL();
+                
+                ExpenseReportBuilder exp = new ExpenseReportBuilder();
 
+                emp = employeeDAL.GetEmployee((Guid)Membership.GetUser().ProviderUserKey);
+
+                Session["EmpDepartment"] = emp.DepartmentId;
+                Session["ExpenseReport"] = expenseReportDAL.GetReportSummaryBySupervisor((int)Session["EmpDepartment"]);
+                GridView1.DataSource = Session["ExpenseReport"];
+                GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int rowindex = Convert.ToInt32(e.CommandArgument.ToString());
+            //// find child gridview control
+            GridView grv = (GridView)GridView1.Rows[rowindex].FindControl("GridView2");
+
+            ////  text for which details display
+          //  Label lbl = (Label)GridView1.Rows[rowindex].FindControl("Label2");
+            GridView1.Rows[rowindex].FindControl("btnCancelItems").Visible = false;
+
+          
+            if (e.CommandName == "Details")
+            {
+                GridView1.Rows[rowindex].FindControl("btnCancelItems").Visible = true;
+                GridView1.Rows[rowindex].FindControl("btnItemDetails").Visible = false;
+
+                ExpenseReportDAL expenseReportDAL = new ExpenseReportDAL();
+                List<ExpenseReport> exp = new List<ExpenseReport>();                
+                exp =(List<ExpenseReport>) Session["ExpenseReport"]; 
+                for(int i=0;i<exp.Count;i++)
+                {
+                    if(exp[i].ExpenseId ==Convert.ToInt32(GridView1.Rows[rowindex].Cells[1].Text))
+                    {
+                    grv.DataSource = exp[i].ExpenseItems;
+                    grv.DataBind();
+                    grv.Visible = true;
+                    break;
+                    }                
+                }
+            }
+            else
+            {
+                //// child gridview  display false when cancel button raise event
+                grv.Visible = false;
+                GridView1.Rows[rowindex].FindControl("btnItemDetails").Visible = true;
+            }
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+           
         }
     }
 }
