b6b051c4f60c6ce834982401858e42c1314b504d
Riki Lingam <riki.lingam@gmail.com>
2014-04-15 20:40:37 +1000

    Updated ExpenseReportDAL

--- ThreeAmigos.ExpenseManagement.DataAccess/ExpenseReportDAL.cs
+++ ThreeAmigos.ExpenseManagement.DataAccess/ExpenseReportDAL.cs
@@ -116,7 +116,7 @@ namespace ThreeAmigos.ExpenseManagement.DataAccess
 
         public List<ExpenseReport> GetExpenseReportsByConsultant(Guid id, string status)
         {
-            string query = String.Format("SELECT ExpenseId, CreateDate, CreatedById, ApprovedById, ProcessedById, Status FROM ExpenseHeader WHERE CreatedById='{0}' and Status LIKE '{1}'", id, status);
+            string query = String.Format("SELECT * FROM ExpenseHeader WHERE CreatedById='{0}' and Status LIKE '{1}'", id, status);
 
             return GetReportsFromDatabase(query);
         }
@@ -125,6 +125,7 @@ namespace ThreeAmigos.ExpenseManagement.DataAccess
         {
             List<ExpenseReport> expenseReports = new List<ExpenseReport>();
             EmployeeDAL employeeDAL = new EmployeeDAL();
+            DepartmentDAL departmentDAL = new DepartmentDAL();
 
             DataAccessFunctions daFunctions = new DataAccessFunctions();
 
@@ -145,10 +146,12 @@ namespace ThreeAmigos.ExpenseManagement.DataAccess
 
                     report.ExpenseId = rdr["ExpenseId"] as int? ?? default(int);
                     report.CreateDate = (DateTime)rdr["CreateDate"];
+                    report.ExpenseToDept = departmentDAL.GetDepartmentProfile(rdr["DepartmentId"] as int? ?? default(int));
                     report.Status = (ReportStatus)Enum.Parse(typeof(ReportStatus), (string)rdr["Status"]);
                     report.CreatedBy = employeeDAL.GetEmployee(rdr["CreatedById"] as Guid? ?? default(Guid));
                     report.ApprovedBy = employeeDAL.GetEmployee(rdr["ApprovedById"] as Guid? ?? default(Guid));
                     report.ProcessedBy = employeeDAL.GetEmployee(rdr["ProcessedById"] as Guid? ?? default(Guid));
+
                     report.ExpenseItems = GetExpenseItemsByExpenseId(report.ExpenseId);
                     expenseReports.Add(report);
                 }
@@ -157,7 +160,7 @@ namespace ThreeAmigos.ExpenseManagement.DataAccess
             }
             catch (Exception ex)
             {
-                throw new Exception("There was a problem running method GetReportSummaryByConsultant: " + ex.Message);
+                throw new Exception("There was a problem retrieving expense reports: " + ex.Message);
             }
 
             return expenseReports;
@@ -171,7 +174,7 @@ namespace ThreeAmigos.ExpenseManagement.DataAccess
 
             DataAccessFunctions daFunctions = new DataAccessFunctions();
 
-            string query = String.Format("SELECT ExpenseHeaderId,ItemId, ExpenseDate, Location, Description,Amount,AudAmount, ReceiptFileName FROM ExpenseItem WHERE ExpenseHeaderId={0}", expenseid); 
+            string query = String.Format("SELECT * FROM ExpenseItem WHERE ExpenseHeaderId={0}", expenseid); 
 
             daFunctions.Connection.Open();
             daFunctions.Command.CommandText = query;
@@ -196,29 +199,7 @@ namespace ThreeAmigos.ExpenseManagement.DataAccess
             daFunctions.Connection.Close();
             return expenseItems;
         }
-
-        //public DataSet GetReportsByConsultant(Guid id, string status)
-        //{
-        //    DataSet expenseReports = new DataSet();
-
-        //    string query = String.Format("SELECT ExpenseId, CreateDate, Status, ItemId, ExpenseDate, Location, Description, AudAmount, ReceiptFileName FROM ExpenseItem i INNER JOIN ExpenseHeader h ON i.ExpenseHeaderId = h.ExpenseId WHERE h.CreatedById ='{0}' AND status LIKE'{1}' ", id, status);
-
-        //    daFunctions.Command.CommandText = query;
-
-        //    SqlDataAdapter adapter = new SqlDataAdapter(query, daFunctions.Connection);
-
-        //    try
-        //    {
-        //        adapter.Fill(expenseReports);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception("There was a problem running method GetReportSummaryByConsultant: " + ex.Message);
-        //    }
-
-        //    return expenseReports;
-        //}
-        
+    
 
         // Below are the methods used by supervisor
         public List<ExpenseReport> GetReportsBySupervisor(int id,string status)
