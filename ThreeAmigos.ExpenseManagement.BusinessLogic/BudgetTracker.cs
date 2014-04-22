34ec7d5e1e8828653b3811cd1f1594b356ae4172
Bikrem <bikrem.singh@yahoo.in>
2014-04-23 03:43:32 +1000

    Added new class "BudgetTracker"

---
+++ ThreeAmigos.ExpenseManagement.BusinessLogic/BudgetTracker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreeAmigos.ExpenseManagement.DataAccess;
+using ThreeAmigos.ExpenseManagement.BusinessObject;
+
+namespace ThreeAmigos.ExpenseManagement.BusinessLogic
+{
+   public class BudgetTracker
+    {
+         ExpenseReportDAL exp = new ExpenseReportDAL();
+
+        public double SumOfExpenseApproved(int DeptId)
+        {
+            return exp.SumOfExpenseApproved(DeptId);
+        }
+
+        // Calculates the budget remaining in a department
+        public double CalculateRemainingBudget(double budget, double totalSpent)
+        {
+            return budget - totalSpent;
+        }
+    }
+}
