bc6253c722a48e896145f42089c6b3abe66e4e17
riki.lingam@gmail.com <riki.lingam@gmail.com>
2014-04-23 15:09:53 +1000

    Updated BudgetTracker and added SpendTracker

--- ThreeAmigos.ExpenseManagement.BusinessLogic/BudgetTracker.cs
+++ ThreeAmigos.ExpenseManagement.BusinessLogic/BudgetTracker.cs
@@ -5,12 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 using ThreeAmigos.ExpenseManagement.DataAccess;
 using ThreeAmigos.ExpenseManagement.BusinessObject;
+using System.Configuration;
 
 namespace ThreeAmigos.ExpenseManagement.BusinessLogic
 {
-   public class BudgetTracker
+    public class BudgetTracker
     {
-         ExpenseReportDAL exp = new ExpenseReportDAL();
+        private double budgetAmount;
+        private double totalExpenseAmount;
+        
+        ExpenseReportDAL exp;
+
+        SpendTrackerDAL spendTracker;
+
+        public BudgetTracker()
+        {
+            exp = new ExpenseReportDAL();
+            spendTracker = new SpendTrackerDAL();
+            budgetAmount = 0;
+            totalExpenseAmount = 0;
+        }
+
+        public double TotalExpenseAmount
+        {
+            get
+            {
+                return totalExpenseAmount;
+            }
+
+            set
+            {
+                if (value > 0)
+                    totalExpenseAmount += value;
+            }
+        }
+
+        public void DepartmentBudget(double deptBudget, int deptId)
+        {
+            budgetAmount = deptBudget;
+            totalExpenseAmount = spendTracker.TotalExpenseAmountByDept(deptId);
+        }
+
+        public void CompanyBudget()
+        {
+            budgetAmount = double.Parse(ConfigurationManager.AppSettings["CompanyMonthlyBudget"]);
+            totalExpenseAmount = spendTracker.TotalExpenseAmountByCompany();
+        }
+
+        public double RemainingAmount()
+        {
+            return budgetAmount - totalExpenseAmount;
+        }
+
+
+
 
         public double SumOfExpenseApproved(int DeptId)
         {
---
+++ ThreeAmigos.ExpenseManagement.DataAccess/SpendTrackerDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreeAmigos.ExpenseManagement.BusinessObject;
+
+
+namespace ThreeAmigos.ExpenseManagement.DataAccess
+{
+    public class SpendTrackerDAL
+    {
+
+        public double TotalExpenseAmountBySupervisor(int supervisorId)
+        {
+            string query = string.Format("SELECT SUM(AudAmount) FROM ExpenseItem i LEFT OUTER JOIN ExpenseHeader h on i.ExpenseHeaderId = h.ExpenseId WHERE h.ApprovedById={0} AND h.Status={1}", supervisorId, ReportStatus.ApprovedByAccountant);
+
+            return GetExpenseTotal(query);        
+        }
+
+        public double TotalExpenseAmountByDept(int deptId)
+        {
+            string query = string.Format("SELECT SUM(AudAmount) FROM ExpenseItem i LEFT OUTER JOIN ExpenseHeader h on i.ExpenseHeaderId = h.ExpenseId WHERE h.DepartmentId={0} AND h.Status={1}", deptId, ReportStatus.ApprovedByAccountant);
+
+            return GetExpenseTotal(query);
+        }
+
+        public double TotalExpenseAmountByCompany()
+        {
+         
+            string query = string.Format("SELECT SUM(AudAmount) FROM ExpenseItem i LEFT OUTER JOIN ExpenseHeader h on i.ExpenseHeaderId = h.ExpenseId WHERE h.Status={0}", ReportStatus.ApprovedByAccountant);
+         
+            return GetExpenseTotal(query);
+        }
+
+        private double GetExpenseTotal(string query)
+        {
+            double totalExpense = 0;
+            DataAccessFunctions daFunctions = new DataAccessFunctions();
+                        
+            daFunctions.Command.CommandText = query;
+
+            totalExpense = daFunctions.Command.ExecuteScalar() as double? ?? default(double);
+
+            return totalExpense;
+        }
+    }
+}
