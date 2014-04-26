2226bcca9d45264fedf70149e3a75c3e11bf8cab
JohnQuan <quan.le.john@gmail.com>
2014-04-26 17:00:54 +1000

    Update method "IsBudgetExceededAccounts(amount)"  inside method "HighlightOverBudget"

--- 32013-Assignment1/Accounts/ProcessExpenses.aspx.cs
+++ 32013-Assignment1/Accounts/ProcessExpenses.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
-//using System.Windows.Forms;
+using System.Windows.Forms;
 using ThreeAmigos.ExpenseManagement.BusinessLogic;
 using ThreeAmigos.ExpenseManagement.BusinessObject;
 using ThreeAmigos.ExpenseManagement.DataAccess;
@@ -43,19 +43,23 @@ namespace ThreeAmigos.ExpenseManagement.UserInterface.Accounts
 
         private void UpdateBudgetMessage()
         {
+            decimal usedAmount = 0;
+            decimal overAmount = 0;
             if (comBudget.RemainingAmount >= 0) 
             {
+                usedAmount = comBudget.BudgetAmount - comBudget.RemainingAmount;
                 lblBudgetMessage.Text =
-                    string.Format("You currently have <b>{0}</b> remaining from the company monthly budget of <b>{1}</b>.",
+                    string.Format("<b>{0}</b> have been approved. You currently have <b>{1} remaining</b> for approval from the company monthly budget of <b>{2}</b>.",
+                    String.Format("{0:c}", usedAmount),
                     String.Format("{0:c}", comBudget.RemainingAmount),
                     String.Format("{0:c}", comBudget.BudgetAmount));
             }
             else
             {
-                decimal overAmount = 0 - comBudget.RemainingAmount;
-                decimal usedAmount = comBudget.BudgetAmount + overAmount;
+                overAmount = 0 - comBudget.RemainingAmount;
+                usedAmount = comBudget.BudgetAmount + overAmount;
                 lblBudgetMessage.Text =
-                    string.Format("You have approved <b>{0}</b>, which is <b>{1}</b> over the company monthly budget of <b>{2}</b>.",
+                    string.Format("<b>{0}</b> have been approved, which is <b>{1} over</b> the company monthly budget of <b>{2}</b>.",
                     String.Format("{0:c}", usedAmount),
                     String.Format("{0:c}", overAmount),
                     String.Format("{0:c}", comBudget.BudgetAmount));
@@ -91,22 +95,18 @@ namespace ThreeAmigos.ExpenseManagement.UserInterface.Accounts
             int expenseId = Convert.ToInt32(arg[0]);
             decimal expenseTotal = Convert.ToDecimal(arg[1]);
 
-            //if (expenseTotal > comBudget.RemainingAmount)
-            //{
-                //DialogResult UserReply = MessageBox.Show("Approving this expense " + expenseTotal + " will cross the total monthly budget of the company. Do you want to approve?", "Important Question", MessageBoxButtons.YesNo);
-                //if (UserReply.ToString() == "Yes")
-                //{
-            expReportBuilder.AccountantActionOnExpenseReport(expenseId, emp.UserId, ReportStatus.ApprovedByAccounts.ToString());
-                //}
-                //else
-                //{
-                    //expReportBuilder.AccountantActionOnExpenseReport(expenseId, emp.UserId, ReportStatus.RejectedByAccountant.ToString());
-            //    }
-            //}
-            //else
-            //{
-            //    expReportBuilder.AccountantActionOnExpenseReport(expenseId, emp.UserId, ReportStatus.ApprovedByAccountant.ToString());
-            //}
+            if (expenseTotal > comBudget.RemainingAmount)
+            {
+                DialogResult UserReply = MessageBox.Show("Approving this expense " + expenseTotal + " will cross the total monthly budget of the company. Do you want to approve?", "Important Question", MessageBoxButtons.YesNo);
+                if (UserReply.ToString() == "Yes")
+                {
+                    expReportBuilder.AccountantActionOnExpenseReport(expenseId, emp.UserId, ReportStatus.ApprovedByAccounts.ToString());
+                }
+            }
+            else                
+            {
+                expReportBuilder.AccountantActionOnExpenseReport(expenseId, emp.UserId, ReportStatus.ApprovedByAccounts.ToString());
+            }
 
             InitializeRepeater();
         }
@@ -119,52 +119,7 @@ namespace ThreeAmigos.ExpenseManagement.UserInterface.Accounts
 
             InitializeRepeater();
         }
-
-        /// <summary>
-        /// Highlight expense report that will result in a department going over budget
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param> 
-        //protected void FormatRepeaterRow(Object sender, RepeaterItemEventArgs e)
-        //{
-        //    if (e.Item.ItemType == ListItemType.AlternatingItem || e.Item.ItemType == ListItemType.Item)
-        //    {
-        //        // get department ID of the expense report
-        //        Label lblDept = e.Item.FindControl("lblDepartmentId") as Label;
-        //        int deptID = Convert.ToInt16(lblDept.Text);  
-                  
-        //        // get the total amount of expenses which were approved by accountant
-        //        SpendTrackerDAL spendTrack = new SpendTrackerDAL();
-        //        decimal deptBudgetProcessed = spendTrack.TotalExpenseAmountByDeptProcessed(deptID);
-        //        //decimal deptBudgetProcessed = deptBudget.SumOfExpenseProcessed(deptID);
-                
-        //        Label lblExpense = e.Item.FindControl("lblExpense") as Label;
-        //        decimal exp = Convert.ToDecimal (lblExpense.Text);  // get the amount of the expense report
-
-        //        // get the monthly budget of the department
-        //        decimal deptBudget = decimal.Parse(ConfigurationManager.AppSettings["DepartmentMonthlyBudget"]);
-
-        //        // get the labels to highlight if over budget
-        //        Label lblDate = e.Item.FindControl("lblDate") as Label; 
-        //        Label lblConsultant = e.Item.FindControl("lblConsultant") as Label;
-        //        Label lblSupervisor = e.Item.FindControl("lblSupervisor") as Label;
-        //        Label lblDepartment = e.Item.FindControl("lblDepartment") as Label;
-        //        Label lblStatus = e.Item.FindControl("lblStatus") as Label;
-        //        Label lblExp = e.Item.FindControl("lblExp") as Label;
-
-        //        // highlight if the expense of the report is more than the remaining budget of the department
-        //        if (exp > deptBudget - deptBudgetProcessed)
-        //        {
-        //            lblDate.BackColor = System.Drawing.Color.Yellow;
-        //            lblConsultant.BackColor = System.Drawing.Color.Yellow;
-        //            lblSupervisor.BackColor = System.Drawing.Color.Yellow;
-        //            lblDepartment.BackColor = System.Drawing.Color.Yellow;
-        //            lblStatus.BackColor = System.Drawing.Color.Yellow;
-        //            lblExp.BackColor = System.Drawing.Color.Yellow;
-        //        }
-        //    } 
-        //}
-
+    
         /// <summary>
         /// Highlight the footer of expense report if the amount will result in the department budget being exceeded
         /// </summary>
@@ -176,7 +131,7 @@ namespace ThreeAmigos.ExpenseManagement.UserInterface.Accounts
             BudgetTracker deptBudget = new BudgetTracker();
             deptBudget.DepartmentBudget(dept.MonthlyBudget,dept.DepartmentId);
 
-            if (deptBudget.IsBudgetExceeded(amount))
+            if (deptBudget.IsBudgetExceededAccounts(amount))
                 return "background-color:#f2dede; color:red";
             else
                 return "background-color:#dff0d8";
